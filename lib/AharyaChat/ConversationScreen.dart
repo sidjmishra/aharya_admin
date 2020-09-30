4dee94668739badfa5f9df87aeb320a3f2bf14b7
Siddhant Mishra <37289521+sidjmishra@users.noreply.github.com>
2020-09-30 22:42:28 +0530

    Update ConversationScreen.dart

--- lib/AharyaChat/ConversationScreen.dart
+++ lib/AharyaChat/ConversationScreen.dart
@@ -47,15 +47,7 @@ class _ConversationScreenState extends State<ConversationScreen> {
     );
   }
 
-  @override
-  void initState() {
-    dataMethods.getConversationMessage(widget.chatRoomId).then((value) {
-      setState(() {
-        chatMessageStream = value;
-      });
-    });
-    super.initState();
-  }
+  // Loading messages before the user could chat
 
   @override
   Widget build(BuildContext context) {
